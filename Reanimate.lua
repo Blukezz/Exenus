67345cfb2523a67b47591e82f6fa974c05b12acd
Blukezz <86196314+Blukezz@users.noreply.github.com>
2023-04-23 17:40:09 -0400

    Update Reanimate.lua

--- Reanimate.lua
+++ Reanimate.lua
@@ -21,7 +21,7 @@ local Global = (getgenv and getgenv()) or getfenv(0) -- globals
 
 --[[
 Global.Settings = {
-    ['Mode'] = 1, -- 1 is defualt (Sukmas hats) and 2 is free 3 is original (gelateks hats) 4 is rectangle head (looks better but more expensive)
+    ['Mode'] = 1, -- 1 is defualt (Sukmas hats) and 2 is free 3 is original (gelateks hats)
     ['CSCollisionsOff'] = false,
     ['AntiSleepBoost'] = 10,
     ['AntiRagdoll'] = true,
